4213d9841c9ec74d02c04ee897e3f586b5fe8d80
Sierra Soto <Sierra_Soto@Uhaul.com>
2021-11-20 12:14:21 -0700

    Haskell
    tic tac toe

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -20,8 +20,6 @@ data Space
     = Open Int 
     | Player Char
     deriving Eq
-
-    
 -- Haskell can automatically make types part of Eq, Ord, Read, Show, Enum, 
 -- and Bounded with the `deriving` keyword at the end of the type declaration
 -- Show for types that can be converted to a string for display
@@ -40,39 +38,54 @@ instance Show Space where
 -- Need to generate a new board to the terminal 
 -- Also check to see if Player X or O won to end the game
 -- Input from Player needs to be an Char and nothing else 
-    --getSpacePosition is a list of Spaces 
-    --making sure the spaces are valid spaces that the player can choose
-getSpacePosition :: [Space] -> IO Int
-getSpacePosition board = do
-    -- do notation chains actions together
-    -- left arrow converts and IO action to a regular value ... so what is right store it to the left
-    input <- getChar 
-    --if input is a single digit, return int if not input again
-        if isDigit && openSpace board (read [input])
-            then return $ (read [input])
-            else do 
-                putStrLn "Enter an open position (1 - 9)"
-                getSpacePosition board
-                where isDigit = input `elem` ['1' .. '9']
 
-
-
--- :: means to tell the complier this expression should be type . . .  type signature
--- ->  -> function with 2 args types that result in IO
-runTicTacToe :: [Space] -> Char -> IO
-runTicTacToe = undefined
 -- remove the Nth number (index being N-1) from a list
 removeNth :: Int -> [a] -> ([a], [a])
 removeNth index lst = (left, right)
     where
         (left, ys) = splitAt (index - 1) lst
         right = drop 1 ys
-
-
+-- Given board, space, and index to place a space in , place space
+--at the position N (index being N - 1)
+--This is to place X, O on the board
+placeSpace :: [a] -> a -> Int -> [a]
+placeSpace board space index = xs ++ [space] ++ ys
+        where (xs, ys) = removeNth index board 
+        --xs is the rest of the list on the left
+        --ys is the rest of the list on the right
+        --Returns true if Space choosen is an open space
+spaceIsOpen :: Space -> Bool
+spaceIsOpen (Open _) = True
+spaceIsOpen _       = False
+-- Return true if the index on the board is open (index is N -1)
+openSpace :: [Space] -> Int -> Bool
+openSpace board index
+    | length board <  i      = False
+    | spaceIsOpen $ board !! i = True
+    | otherwise              = False
+    where i = index - 1 
 -- To make a type a member of type class the instance keyword is used
 --Instance shows how the behavior implements using the given data
+ --getSpacePosition is a list of Spaces 
+    --making sure the spaces are valid spaces that the player can choose
+getSpacePosition :: [Space] -> IO Int
+getSpacePosition board = do
+    putStrLn "Enter an open position (1 - 9):"
+        -- do notation chains actions together
+        -- left arrow converts and IO action to a regular value ... so what is right store it to the left
+    input <- getChar 
+        --if input is a single digit, return int if not input again
+            --is this space taken up already
+    if input `elem` ['1' .. '9'] && openSpace board (read [input])
+        then return $ (read [input])
+        else getSpacePosition board
 
 
+               
+-- :: means to tell the complier this expression should be type . . .  type signature
+-- ->  -> function with 2 args types that result in IO
+runTicTacToe :: [Space] -> Char -> IO ()
+runTicTacToe board playerChr = undefined
 -- When a Haskell program is executed, `main` is
 -- called. It must return a value of type `IO a` for some type
 main :: IO ()
